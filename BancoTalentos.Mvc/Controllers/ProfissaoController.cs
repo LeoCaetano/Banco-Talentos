5242f44a2b8d7a33386f9d1190a54b729b97f137
LeoCaetano <leonardocaetano95@yahoo.com.br>
2021-06-01 19:09:51 -0300

    Crud Básico de Profissões

--- BancoTalentos.Mvc/Controllers/ProfissaoController.cs
+++ BancoTalentos.Mvc/Controllers/ProfissaoController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BancoTalentos.Mvc.Models;
 using BancoTalentos.Dados;
+using BancoTalentos.Modelos.Models;
 
 namespace BancoTalentos.Mvc.Controllers
 {
@@ -20,6 +21,44 @@ namespace BancoTalentos.Mvc.Controllers
         public IActionResult Consulta(){
             var profissoes = _contexto.ProfissaoDbSet.ToList();
             return View(profissoes);
+        }
+
+        public IActionResult Cadastro(){
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(Profissao profissao)
+        {
+
+            if(profissao.IdProfissao > 0){
+                Profissao prof = _contexto.ProfissaoDbSet.First(x => x.IdProfissao == profissao.IdProfissao);
+                prof.Nome = profissao.Nome;
+                prof.Ativo = profissao.Ativo;
+            }else
+                _contexto.ProfissaoDbSet.Add(profissao);
+            
+            await _contexto.SaveChangesAsync();
+            
+            return RedirectToAction("Consulta");
+        }
+
+        [HttpGet]
+         public IActionResult Editar(int id)
+        {
+            var profissao = _contexto.ProfissaoDbSet.First(c => c.IdProfissao == id);
+            
+            return View("Cadastro", profissao);
+        }
+
+        public async Task<IActionResult> Deletar(int id)
+        {
+            var profissao = _contexto.ProfissaoDbSet.First(c => c.IdProfissao == id);
+            _contexto.ProfissaoDbSet.Remove(profissao);
+            await _contexto.SaveChangesAsync();
+            
+            return RedirectToAction("Consulta");
         }
         
     }
